a13db6d19f591bce3c9529fe3406013bd506596a
J-Montgom <47634024+J-Montgom@users.noreply.github.com>
2022-07-12 20:55:04 -0400

    Made password

--- ComputerCraft/Password Terminal.lua
+++ ComputerCraft/Password Terminal.lua
@@ -1,3 +1,19 @@
 -- Password Terminal
 
 -- File needs to be named startup in terminal so it doesn't shutoff due to chunk loading 
+
+local pass = "SOMA"
+
+while true do
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Enter password: ")
+    input = read()
+    if input == pass then
+        redstone.setOutput("back", true)
+        sleep(5)
+        redstone.setOutput("back", false)
+    else
+        print("Wrong password")
+    end
+end

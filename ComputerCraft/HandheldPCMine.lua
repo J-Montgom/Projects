4c18d1d2c09a4563658e3c21a95f040c856fc1d7
J-Montgom <47634024+J-Montgom@users.noreply.github.com>
2022-07-12 21:58:20 -0400

    Begun the project

--- ComputerCraft/HandheldPCMine.lua
+++ ComputerCraft/HandheldPCMine.lua
@@ -1,5 +1,6 @@
 -- This is for the handheld computer with endermodem
 -- Requires a 4 computer beacon to locate gps coordinates, sends coordinates to turtles, will set this up for 5 turtles
+-- -Z is North, +X is East
 
 --[[
     Best levels for each ore:
@@ -8,4 +9,10 @@
     Redstone/Diamond/Lapis Y = -50
 ]]
 
-global x,y,z = gps.locate
+-- 5 is the time before it times out
+
+local x,y,z = gps.locate(5)
+local location = vector.new(gps.locate(5))
+
+print("You are located at: ".. location)
+

38b3c4eff52f955059f76b63cb5450af853dae68
J-Montgom <47634024+J-Montgom@users.noreply.github.com>
2022-07-12 21:27:23 -0400

    Comments added to remind me

---
+++ ComputerCraft/turtle..lua
@@ -0,0 +1,11 @@
+-- This is for the handheld computer with endermodem
+-- Requires a 4 computer beacon to locate gps coordinates, sends coordinates to turtles, will set this up for 5 turtles
+
+--[[
+    Best levels for each ore:
+    Iron Y = 16
+    Gold Y = -16
+    Redstone/Diamond/Lapis Y = -50
+]]
+
+global x,y,z = gps.locate
